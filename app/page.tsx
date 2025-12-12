b22241233952004216b1abb105c8a11da256036f
dahal-manish <danish.mahal10@gmail.com>
2025-12-11 19:55:07 -0500

    update text style for name heading home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl font-sans mb-6">Manish Dahal</h1>
+      <h1 className="text-5xl font-serif mb-6">Manish Dahal</h1>
       <div className="space-y-4 text-lg leading-relaxed">
         <p>
           Manish Dahal is a Nepal-born, Charlottesville-raised first-generation college graduate striving to create a world where underrepresented students have equal opportunities and a community that empowers them to become pioneers. He is a Software Engineer at Capital One and has previously worked across startups, private equity, venture capital, and real estate. His passions lie at the nexus of education, technology, and policy.
